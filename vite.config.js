f03e872a837d5ff676ce81c205a5a69c4e410a2a
413D3Z <28901773+413D3Z@users.noreply.github.com>
2024-06-19 09:38:04 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js', 'resources/css/app.css', 'resources/scss/style.scss'],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    build: {
+        sourcemap: true,
+        rollupOptions: {
+            external: ['animate.css'],
+            output: {
+                manualChunks: {
+                    vue: ['vue'],
+                    inertia: ['@inertiajs/inertia', '@inertiajs/inertia-vue3'],
+                },
+            },
+        },
+    },
+});
