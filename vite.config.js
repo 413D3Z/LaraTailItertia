7c3dfaa55775b6ca22d0371ef42d76018650bea3
413D3Z <28901773+413D3Z@users.noreply.github.com>
2024-06-19 10:03:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'resources/js'),
+            ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
         },
     },
     server: {
